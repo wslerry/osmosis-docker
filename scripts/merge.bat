7e689ea376693e80133fc53a365dda3704a3af95
Lerry William Seling <lerryws@sains.com.my>
2024-06-23 23:59:19 +0800

    add symlink scripts for linux

--- scripts/merge.bat
+++ scripts/merge.bat
@@ -2,6 +2,6 @@
 docker run -ti --rm -e JAVACMD_OPTIONS="-Xmx5g -Xms5g" \
 -e OSM_SEA_FILE=sea.osm \
 -e OSM_PBF_FILE=malaysia-singapore-brunei-latest.osm.pbf \
--v ${PWD}/data:/data/input/ \
+-v ${PWD}/osm/data:/data/input/ \
 -v ${PWD}/output:/data/output/ \
 lerryws/osmosis merge
