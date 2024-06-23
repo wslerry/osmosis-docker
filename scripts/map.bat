f587cfc84ce6a7c39fcfe7cd032ad9329ab9087a
Lerry William Seling <lerryws@sains.com.my>
2024-06-23 23:32:48 +0800

    add symlink scripts for linux

---
+++ scripts/map.bat
@@ -0,0 +1,7 @@
+@echo off
+docker run -ti --rm -e JAVACMD_OPTIONS="-Xmx5g -Xms5g" \
+-e OSM_SEA_FILE=sea.osm \
+-e OSM_PBF_FILE=malaysia-singapore-brunei-latest.osm.pbf \
+-v ../data:/data/input/ \
+-v ../output:/data/output/ \
+lerryws/osmosis map
