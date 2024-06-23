d5a32cbb4bde598356a669c41429be5d88a3d762
Lerry William Seling <lerryws@sains.com.my>
2024-06-23 23:41:45 +0800

    add symlink scripts for linux

--- scripts/clip.bat
+++ scripts/clip.bat
@@ -1,2 +1,2 @@
 @echo off
-docker run -it --rm -e JAVACMD_OPTIONS="-Xmx5g -Xms5g" -e OSM_SEA_FILE=sea.osm -e OSM_PBF_FILE=malaysia-singapore-brunei-latest.osm.pbf -v ../data:/data/input/ -v ../output:/data/output/ lerryws/osmosis clip
+docker run -it --rm -e JAVACMD_OPTIONS="-Xmx5g -Xms5g" -e OSM_SEA_FILE=sea.osm -e OSM_PBF_FILE=malaysia-singapore-brunei-latest.osm.pbf -v ${PWD}/data:/data/input/ -v ${PWD}/output:/data/output/ lerryws/osmosis clip
